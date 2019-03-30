a2e484b4c137da451a46d497909777b3676ca199
Tomasz Odrzywolek <tomeko92@gmail.com>
2019-03-30 01:28:16 +0100

    add gradle add spring dependencies to project update README and .gitignore

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # bus-registration-system
+
+To build project run command `./gradlew build`
+
+To run project run command `./gradlew bootRun`
+
+To run tests run command `./gradlew test`
+
+App listens on port `8080`
---
+++ src/main/java/pl/edu/agh/to/busregistration/Application.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to.busregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
