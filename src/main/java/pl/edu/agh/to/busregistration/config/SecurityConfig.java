54cf85ea6f7ac1a47e1be39a463872b72f9e798e
Tomasz Odrzywolek <tomeko92@gmail.com>
2019-05-10 23:54:00 +0200

    app security

---
+++ src/main/java/pl/edu/agh/to/busregistration/config/SecurityConfig.java
@@ -0,0 +1,40 @@
+package pl.edu.agh.to.busregistration.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+        auth.inMemoryAuthentication()
+                .withUser(users.username("user").password("password").roles("USER"));
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("admin").password("admin").roles("ADMIN"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/css/**", "/js/**").permitAll();
+
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .loginProcessingUrl("/authenticate")
+                .permitAll();
+    }
+}
--- src/main/java/pl/edu/agh/to/busregistration/controllers/LoginController.java
+++ src/main/java/pl/edu/agh/to/busregistration/controllers/LoginController.java
@@ -10,24 +10,8 @@ import pl.edu.agh.to.busregistration.forms.LoginForm;
 @Controller
 public class LoginController {
 
-    @GetMapping(value = {"/", "/login"})
-    public String index() {
-        return "index";
-    }
-
-    @PostMapping("/login")
-    public String login(@ModelAttribute(name = "loginForm") LoginForm loginForm, Model model) {
-
-        String username = loginForm.getUsername();
-        String password = loginForm.getPassword();
-
-        if ("admin".equals(username) && "admin".equals(password)) {
-            return "admin_home";
-        } else if ("user".equals(username) && "password".equals(password)) {
-            return "home";
-        } else {
-            model.addAttribute("invalidCredentials", true);
-            return "index";
-        }
+    @GetMapping("/login")
+    public String login() {
+        return "/login";
     }
 }
