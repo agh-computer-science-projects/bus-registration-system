c933dd83e5650f4e959f85c262f2dce93454d142
Tomasz Odrzywolek <tomeko92@gmail.com>
2019-05-11 09:35:46 +0200

    working on app security

--- src/main/java/pl/edu/agh/to/busregistration/config/SecurityConfig.java
+++ src/main/java/pl/edu/agh/to/busregistration/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package pl.edu.agh.to.busregistration.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -35,6 +34,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/authenticate")
+                .successForwardUrl("/handle-login")
+                .permitAll()
+                .and()
+                .logout()
                 .permitAll();
     }
 }
--- src/main/java/pl/edu/agh/to/busregistration/controllers/LoginController.java
+++ src/main/java/pl/edu/agh/to/busregistration/controllers/LoginController.java
@@ -1,5 +1,7 @@
 package pl.edu.agh.to.busregistration.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,23 @@ public class LoginController {
 
     @GetMapping("/login")
     public String login() {
-        return "/login";
+        return "login";
+    }
+
+    @PostMapping("/handle-login")
+    public String handleLogin() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String role = auth.getAuthorities().toString();
+
+        String targetUrl = "home";
+        if(role.contains("ADMIN")) {
+            targetUrl = "admin_home";
+        }
+        return targetUrl;
+    }
+
+    @GetMapping("/admin-home")
+    public String adminHome() {
+        return "admin_home";
     }
 }
