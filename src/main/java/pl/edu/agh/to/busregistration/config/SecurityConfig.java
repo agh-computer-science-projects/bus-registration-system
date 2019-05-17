b649c3a5fc90ea596e127b378a2bdccabcafa500
Tomasz Odrzywolek <tomeko92@gmail.com>
2019-05-17 02:14:50 +0200

    remove security for static files (css, js, img)

--- src/main/java/pl/edu/agh/to/busregistration/config/SecurityConfig.java
+++ src/main/java/pl/edu/agh/to/busregistration/config/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 
         http.authorizeRequests()
-                .antMatchers("/css/**", "/js/**").permitAll();
+                .antMatchers("/css/**", "/js/**", "/images/**").permitAll();
 
         http.authorizeRequests()
                 .anyRequest().authenticated()
